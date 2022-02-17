de0cbe1e9907f40c987df3bdca4a1e3bcbaa68f0
Bahadır <bahadirtilki@outlook.com>
2022-02-17 10:41:31 +0300

    main

--- src/main/java/frc/robot/commands/TurretBangBangController.java
+++ src/main/java/frc/robot/commands/TurretBangBangController.java
@@ -31,7 +31,9 @@ public class TurretBangBangController extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    turret.runTurret(0);
+  }
 
   // Returns true when the command should end.
   @Override
